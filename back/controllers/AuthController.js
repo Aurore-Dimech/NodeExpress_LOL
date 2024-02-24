6bacc9c367e478196280da1a5db016c560a0c98a
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2024-02-24 15:43:34 +0100

    FXED double counting of users

--- back/controllers/AuthController.js
+++ back/controllers/AuthController.js
@@ -8,24 +8,38 @@ const signUp = async (req, res) => {
     const { email, pseudo, password } = req.body
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    prisma.user.create({
-        data: {
-            email,
-            pseudo,
-            password: hashedPassword
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email
         }
     })
 
-    .then((user) => {
-        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { 
-            expiresIn: '2h'
+    if (existingUser) {
+        return res.status(400).json({ error: 'This email is already registered' })
+    }
+
+    if(email === '' || pseudo === '' || password ===''){
+        res.json({error: 'All fields must be completed'})
+    } else {
+        prisma.user.create({
+            data: {
+                email,
+                pseudo,
+                password: hashedPassword
+            }
         })
     
-        res.json(token)
-    })
-    .catch((error) => {
-        res.json({error: error.message})
-    })
+        .then((user) => {
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { 
+                expiresIn: '2h'
+            })
+        
+            res.json(token)
+        })
+        .catch((error) => {
+            res.json({error: error.message})
+        })
+    }
 }
 
 const logIn = async (req, res) => {
