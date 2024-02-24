eee21b1c0eed158bc8a60059335c0645d10e68a2
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2024-02-24 13:34:52 +0100

    AUTHENTIFICATION system added

---
+++ back/controllers/AuthController.js
@@ -0,0 +1,56 @@
+import { PrismaClient } from "@prisma/client"
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
+const prisma = new PrismaClient()
+
+const signUp = async (req, res) => {
+    const { email, pseudo, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    prisma.user.create({
+        data: {
+            email,
+            pseudo,
+            password: hashedPassword
+        }
+    })
+
+    .then((user) => {
+        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { 
+            expiresIn: '2h'
+        })
+    
+        res.json(token)
+    })
+    .catch((error) => {
+        res.json({error: error.message})
+    })
+}
+
+const logIn = async (req, res) => {
+    const { email, password } = req.body
+    const user = await prisma.user.findUnique({
+        where : {
+            email
+        }
+    })
+
+    if (!user){
+        return res.json({error: 'User not found'})
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.json({error: 'Password not valid'})
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: '2h'
+    })
+
+    res.json(token)
+}
+
+export { signUp, logIn }
--- back/router.js
+++ back/router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import characters from './routes/CharacterRoutes.js'
+import auth from './routes/AuthRoutes.js'
 
 const router = express.Router();
 
 router.use('/characters', characters) 
+router.use('/auth', auth)
 
 export default router
